1a8ac0ace4673e0ec978b3011b943f3fec60c7f9
vijayforos <vijayuppalapatiforos@gmail.com>
2018-03-14 14:35:16 -0400

    verion 1.0.0 removed hard coded nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,21 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-    'hostname' => 'ec2-54-89-238-106.compute-1.amazonaws.com',
-    'ipaddress' => '54.89.238.106',
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
     'port' => 80,
     'ssl_port' => 80
-  },{
-    'hostname' => 'ec2-34-228-224-42.compute-1.amazonaws.com',
-    'ipaddress' => '34.228.224.42',
-    'port' => '80',
-    'ssl_port' => '80'
-    }
-]
+  }
+  members.push(member)
+
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
