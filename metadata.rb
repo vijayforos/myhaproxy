a08e9cb52c74fe639684fe419a3771257b4949d1
vijayforos <vijayuppalapatiforos@gmail.com>
2018-03-14 10:40:04 -0400

    verion 0.2.0 added new apache node to lb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures myhaproxy'
 long_description 'Installs/Configures myhaproxy'
-version '0.1.1'
+version '0.2.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,12 @@ node.default['haproxy']['members'] = [{
     'ipaddress' => '54.89.238.106',
     'port' => 80,
     'ssl_port' => 80
-  }]
+  },{
+    'hostname' => 'ec2-34-228-224-42.compute-1.amazonaws.com',
+    'ipaddress' => '34.228.224.42',
+    'port' => '80'
+    'ssl_port' => '80'
+    }
+]
 
 include_recipe 'haproxy::manual'
