e2fd89e50cb83e11ac6598892591d9f4bbaedce9
vijayforos <vijayuppalapatiforos@gmail.com>
2018-03-14 09:59:02 -0400

    Working version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures myhaproxy'
 long_description 'Installs/Configures myhaproxy'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,4 @@ chef_version '>= 12.14' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/myhaproxy'
+depends 'haproxy', '~> 3.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-54-89-238-106.compute-1.amazonaws.com',
+    'ipaddress' => '54.89.238.106',
+    'port' => 80,
+    'ssl_port' => 80
+  }]
+
+include_recipe 'haproxy::manual'
